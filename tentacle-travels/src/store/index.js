7843943fbec5eb261740d1681e15a1c7bce5c622
Ainebyoona Dickson <92643336+Aine-dickson@users.noreply.github.com>
2023-05-10 12:13:24 +0300

    initial commit

---
+++ tentacle-travels/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: {
+    // Define your application's state here
+  },
+  mutations: {
+    // Define your state mutations here
+  },
+  actions: {
+    // Define your actions here
+  },
+  modules: {
+    // Define your modules here
+  }
+})
+
+export default store
